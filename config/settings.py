ea641fdb7379a8b1e40f2a187937ef9086d7fd10
Axel Rayet <axel.rayet@protonmail.com>
2022-06-21 17:57:37 +0200

    Changed database to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "coinspace",
         "USER": os.environ["DB_USER"],
         "PASSWORD": os.environ["DB_PWD"],
         "HOST": "localhost",
-        "PORT": "3306",
+        "PORT": "5432",
     }
 }
 
